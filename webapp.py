a207167eeda3a403f6aa3a783f7514bffd989b59
JamisonSavelleno <jamison2006@icloud.com>
2024-04-05 13:05:12 -0700

    added the UI bar and integrated the home page into it. Also added a blank new page for facts.

--- webapp.py
+++ webapp.py
@@ -1,80 +1,13 @@
-from flask import Flask, request, render_template, flash
-from markupsafe import Markup
+from flask import Flask, url_for, render_template, request
 
-import os
-import json
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    states = get_state_options()
-    #print(states)
-    return render_template('home.html', state_options=states)
-
-@app.route('/showFact')
-def render_fact():
-    states = get_state_options()
-    state = request.args.get('state')
-    county = county_most_under_18(state)
-    fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
-    return render_template('home.html', state_options=states, funFact=fact)
-
-@app.route('/showFact2')
-def render_fact2():
-    states = get_state_options()
-    state = request.args.get('state')
-    vets = get_most_vets(state)
-    fact2 = "In " + state + ", the county with the most veterans is " + county + "."
-    return render_template('home.html', state_options=states, funFact2=fact2)
+@app.route("/")
+def render_main():
+    return render_template('home.html')    
     
-def get_state_options():
-    """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
-    with open('demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    states=[]
-    for c in counties:
-        if c["State"] not in states:
-            states.append(c["State"])
-    options=""
-    for s in states:
-        options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
-    return options
-    
-def get_most_vets(state):
-    """Return the name of a county in the given state with the highest number of vets."""
-    with open('demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    highest=0
-    county = ""
-    for c in counties:
-        if c["State"] == state:
-            if c["Miscellaneous"]["Veterans"] > highest:
-                highest = c["Miscellaneous"]["Veterans"]
-                county = c["County"]
-    return county
-
-def county_most_under_18(state):
-    """Return the name of a county in the given state with the highest percent of under 18 year olds."""
-    with open('demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    highest=0
-    county = ""
-    for c in counties:
-        if c["State"] == state:
-            if c["Age"]["Percent Under 18 Years"] > highest:
-                highest = c["Age"]["Percent Under 18 Years"]
-                county = c["County"]
-    return county
-
-def is_localhost():
-    """ Determines if app is running on localhost or not
-    Adapted from: https://stackoverflow.com/questions/17077863/how-to-see-if-a-flask-app-is-being-run-on-localhost
-    """
-    root_url = request.url_root
-    developer_url = 'http://127.0.0.1:5000/'
-    return root_url == developer_url
-
-
-if __name__ == '__main__':
-    app.run(debug=False) # change to False when running in production
+@app.route("/p1")
+def render_page1():
+    return render_template('page1.html') 
+if __name__=="__main__":
+    app.run(debug=False)

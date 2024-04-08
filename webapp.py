f323ece075619b2fc468a679a983ff2d039d8cc4
JamisonSavelleno <jamison2006@icloud.com>
2024-04-08 14:18:04 -0700

    dropdown menu works, can use selected value. Getting the fact to work is the project rn

--- webapp.py
+++ webapp.py
@@ -12,20 +12,33 @@ def render_main():
     
 @app.route("/p1")
 def render_page1():
-    return render_template('page1.html')
+    quakes = get_quake_options()
+    return render_template('page1.html', location_options = quakes)
 
 @app.route('/showFact')
 def render_fact():
-    quakes = []
     quakes = get_quake_options()
-    return render_template('page1.html', location_options = quakes)    
+    quakeFacts = get_quake_list()
+    state = request.args["state"]
+    inputVar = []
+    for a in quakeFacts:
+        if a["location"]["full"] == state:
+            inputVar.append(a)
+    info = "This is magnitude " + inputVar["impact"]["magnitude"] + " earthquake and a significance of " + inputVar["impact"]["significance"] + ". It has a gap of" + inputVar["impact"]["gap"] + ". It had a depth of " + inputVar["location"]["magnitude"] + " and was located at a latitude of" + inputVar["location"]["latitude"] + " and a longitude of " + inputVar["location"]["longitude"] + ". It happened on " + inputVar["time"]["month"] + "/" + inputVar["time"]["day"] + "/" + inputVar["time"]["year"] + " at " + inputVar["time"]["hour"] + ":" + inputVar["time"]["minute"] + "O'clock."
+    return render_template('page1.html', location_options = quakes, info = info) 
+
+def get_quake_list():
+    """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
+    with open('earthquakes.json') as earthquakes_data:
+        data = json.load(earthquakes_data)
+    return data    
     
 def get_quake_options():
     """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
     with open('earthquakes.json') as earthquakes_data:
         data = json.load(earthquakes_data)
     quakes_loc = []
-    for a in quakes_loc:
+    for a in data:
         quakes_loc.append(a["location"]["full"])
     options=""
     for s in quakes_loc:
